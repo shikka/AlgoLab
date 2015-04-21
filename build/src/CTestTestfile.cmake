d74c6e771e5f593ac5494ba12dd73a22d94dc3ba
shikka <marbo4@gmx.de>
2015-04-22 01:53:50 +0200

    fix gitignore

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shikka/Studium/master/2.Semester/Labor Algorithmen/Labor/src
-# Build directory: /home/shikka/Studium/master/2.Semester/Labor Algorithmen/Labor/build/src
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
